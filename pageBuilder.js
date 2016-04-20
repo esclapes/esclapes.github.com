e40f7a96b357e92e99650cc132413148c135a66f
Eduardo Esclapes <eduardo.esclapes@gmail.com>
2016-04-20 08:37:30 +0200

    slug, comments, content scrapping

--- pageBuilder.js
+++ pageBuilder.js
@@ -1,52 +1,69 @@
 var toMd 		= require('to-markdown');
 var cheerio 	= require('cheerio');
 var fs 			= require('fs');
-var slug        = require('slugg');
+var slug        = require('slug');
 
-exports.Page = function(file){
+var Page = function(file){
     this._$ = cheerio.load(fs.readFileSync(file), { ignoreWhitespace: true });
-    this.parts = {};
     var id = file.match(/node\/(\d+)\.html/);
     if (id) {
-        this.parts.id = id[1];
-        this.parts.date = this.parseDate(this._$('.article .submitted .time').first().text());
-        this.parts.title = this._$('#page-title').first().text();
-        this.parts.content = this.parseContent();
-        this.parts.comments = this.parseComments();
+       this.parts = this.getParts();
+       this.parts.id = id[1];
     }
     else {
         throw new Error('Not a node');
     }
 };
 
-exports.Page.prototype.parseDate = function(datestring) {
+Page.prototype.getParts = function() {
+  var parts = {};
+  parts.date = this.parseDate(this._$('.article .submitted .time').first().text());
+  parts.title = this._$('#page-title').first().text();
+  parts.slug = parts.date + '-' + slug(parts.title, {lower: true});
+  parts.content = this.parseContent();
+  parts.tags = this._$('.vocab ul li').text().split(' | ');
+  parts.comments = this.parseComments();
+
+  return parts;
+}
+
+Page.prototype.parseDate = function(datestring) {
     var date = datestring.match(/(\d+)\/(\d+)\/(\d+)/);
     // We return an ordered date for Jekyll
     return date ? [date[3],date[2],date[1]].join('-') : '' ;
 };
 
-exports.Page.prototype.parseContent = function(){
+Page.prototype.parseContent = function(){
 	var raw = this._$('#content .article').clone();
 		raw.find('ul.links').remove();
 		raw.find('p.submitted').remove();
+    raw.find('.vocab').remove();
+    var self = this;
+    raw.find('span, p, div').each(function() {
+      if ( '' === self._$( this ).html().trim() ) {
+        self._$( this ).remove();
+      }
+    });
 		return raw.html() ? toMd(raw.html()) : '';
 }
 
-exports.Page.prototype.parseComments = function(){
+Page.prototype.parseComments = function(){
     var self = this;
     var comments = [];
     self._$('#comments .comment').each(function(i, elem) {
         comments[i] = {}
-        comments[i].author = self._$(this).find('.author a').first().text();
-        comments[i].date = self.parseDate(self._$(this).find('.submitted time').first().text()),
+        comments[i].author = self._$(this).find('.author').first().text();
+        comments[i].date = self.parseDate(self._$(this).find('.time').first().text()),
         self._$(this).find('.submitted').remove();
         self._$(this).find('.links').remove();
-        comments[i].comment = self._$(this).html() ? toMd(self._$(this).html()) : '';
-        
+        comments[i].comment = self._$(this).text();
+
     });
         return comments;
 }
 
-exports.Page.prototype.log = function(){
+Page.prototype.log = function(){
    	console.log(this.parts);
 };
+
+exports.Page = Page;
--- tomarkdown.js
+++ tomarkdown.js
@@ -1,26 +1,33 @@
 #!/usr/bin/env node
 
-var dive = require('dive'),
+var fs = require('fs'),
+    dive = require('dive'),
+    swig = require('swig'),
     pageBuilder = require('./pageBuilder.js'),
+    pages = [],
     notFound = [];
- 
+
 dive('./node', { all: true }, function(err, file) {
   if (err) throw err;
-  
+
   try {
   	var page = new pageBuilder.Page(file);
-  	if (page.parts.id == '99') {
-  		page.log();
+  	if (page.parts.id) {
+  		pages.push(page);
   	}
   	else {
   		//page.log();
   	}
   }
   catch(E) {
-  	//console.log(E);
-	notFound.push('No page for ' + file);
+     console.log(E);
+	   notFound.push('No page for ' + file);
   }
-  
+
 }, function() {
-  console.log(notFound);
+    var template = swig.compileFile('./template.swig');
+    pages.forEach(function(page) {
+        fs.writeFile('./minimal/_posts/'+page.parts.slug+'.md', template(page.parts));
+    });
+
 });
